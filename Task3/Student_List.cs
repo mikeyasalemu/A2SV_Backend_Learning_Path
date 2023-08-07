29b71950986552ce87f47bc0861ec71a313e69dd
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-08-07 10:24:14 +0300

    Update Student_List.cs

--- Task3/Student_List.cs
+++ Task3/Student_List.cs
@@ -2,59 +2,47 @@ using System;
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-
+using System.Linq;
 public class StudentList
 {
-    private string fileName = "JsonFile.json";
-    public List<Student> studentList;
+    private List<Student> studentList;
 
     public StudentList()
     {
         studentList = new List<Student>();
-        DeserializeFromFile();
     }
 
-    private void SerializeToFile()//
+    public void AddStudent(Student student)
+    {
+        studentList.Add(student);
+    }
+
+    public Student SearchStudent(Func<Student, bool> predicate)
+    {
+        return studentList.FirstOrDefault(predicate);
+    }
+
+    public void DisplayAllStudents()
+    {
+        foreach (var student in studentList)
+        {
+            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, " +
+                              $"Roll Number: {student.RollNumber}, Grade: {student.Grade}");
+        }
+    }
+
+    public void SerializeToJson(string filePath)
     {
         string jsonString = JsonConvert.SerializeObject(studentList, Formatting.Indented);
-        File.WriteAllText(fileName, jsonString);
+        File.WriteAllText(filePath, jsonString);
     }
 
-    private void DeserializeFromFile()
+    public void DeserializeFromJson(string filePath)
     {
-        if (!File.Exists(fileName))
+        if (File.Exists(filePath))
         {
-            // Create an empty JSON array in the file
-            File.WriteAllText(fileName, "[]");
-        }
-
-        string jsonString = File.ReadAllText(fileName);
-        List<Student> deserializedFile = JsonConvert.DeserializeObject<List<Student>>(jsonString);
-        if (deserializedFile != null)
-            studentList = deserializedFile;
-    }
-
-    public void AddStudent(string name, int age, string rollNumber, double grade)
-    {
-        studentList.Add(new Student(rollNumber) { Name = name, Age = age, Grade = grade });
-        SerializeToFile();
-    }
-
-    public void SearchStudent(string name)
-    {
-        List<Student> students = (from student in studentList where student.Name == name select student).ToList();
-        DisplayStudents(students);
-    }
-
-    public void DisplayStudents(List<Student>? students = null)
-    {
-        if (students == null)
-            students = studentList;
-        Console.WriteLine("STUDENTS LIST:");
-        Console.WriteLine("{0,-10}{1,-10}{2,-15}{3}", "NAME", "AGE", "ROLL NUMBER", "GRADE");
-        foreach (Student student in students)
-        {
-            Console.WriteLine("{0,-10}{1,-10}{2,-15}{3}", student.Name, student.Age, student.RollNumber, student.Grade);
+            string jsonString = File.ReadAllText(filePath);
+            studentList = JsonConvert.DeserializeObject<List<Student>>(jsonString) ?? new List<Student>();
         }
     }
 }
