c6c02af7d2648811e4c8dfd61d6c7994a4d2af7a
mikeyasalemu <88858460+mikeyasalemu@users.noreply.github.com>
2023-08-07 10:25:27 +0300

    Update Program.cs

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,26 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-// Task1.Main();
-Day2T1.Main();
-=======
-using System;
-using System.IO;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
 public class Program
 {
-    public static void Main(string[] args){
+    public static void Main(string[] args)
+    {
+        const string filePath = "studentData.json";
+        StudentList studentList = new StudentList();
+        bool exit = false;
 
-        StudentList students = new StudentList();
+        Console.WriteLine("Student Management System");
+        Console.WriteLine("*************************");
 
-        students.AddStudent("AAA",23,"AA4565",3.4);
-        students.AddStudent("BBB",22,"BB4565",2.4);
+        while (!exit)
+        {
+            Console.WriteLine("\nWelcome! Please select an option:");
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. Search Student by Name");
+            Console.WriteLine("3. Search Student by Roll Number");
+            Console.WriteLine("4. Display All Students");
+            Console.WriteLine("5. Save Data to JSON");
+            Console.WriteLine("6. Load Data from JSON");
+            Console.WriteLine("7. Exit");
 
-        students.SearchStudent("AAA");
-        
-        students.DisplayStudents();
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter Age: ");
+                    int age = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Roll Number: ");
+                    string rollNumber = Console.ReadLine();
+                    Console.Write("Enter Grade: ");
+                    double grade = double.Parse(Console.ReadLine());
+
+                    var newStudent = new Student(rollNumber) { Name = name, Age = age, Grade = grade };
+                    studentList.AddStudent(newStudent);
+                    Console.WriteLine("Student added successfully.");
+                    break;
+
+                case "2":
+                    Console.Write("Enter the name to search: ");
+                    string searchName = Console.ReadLine();
+                    var resultByName = studentList.SearchStudent(s => s.Name == searchName);
+                    if (resultByName != null)
+                    {
+                        Console.WriteLine("Student Found:");
+                        Console.WriteLine($"Name: {resultByName.Name}, Age: {resultByName.Age}, " +
+                                        $"Roll Number: {resultByName.RollNumber}, Grade: {resultByName.Grade}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No student found with the given name.");
+                    }
+                    break;
+
+                case "3":
+                    Console.Write("Enter the Roll Number to search: ");
+                    string searchRollNumber = Console.ReadLine();
+                    var resultByRollNumber = studentList.SearchStudent(s => s.RollNumber == searchRollNumber);
+                    if (resultByRollNumber != null)
+                    {
+                        Console.WriteLine("Student Found:");
+                        Console.WriteLine($"Name: {resultByRollNumber.Name}, Age: {resultByRollNumber.Age}, " +
+                                        $"Roll Number: {resultByRollNumber.RollNumber}, Grade: {resultByRollNumber.Grade}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No student found with the given Roll Number.");
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("All Students:");
+                    studentList.DisplayAllStudents();
+                    break;
+
+                case "5":
+                    studentList.SerializeToJson(filePath);
+                    Console.WriteLine("Data saved to JSON successfully.");
+                    break;
+
+                case "6":
+                    studentList.DeserializeFromJson(filePath);
+                    Console.WriteLine("Data loaded from JSON successfully.");
+                    break;
+
+                case "7":
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
->>>>>>> 8c4b418 (task 3)
